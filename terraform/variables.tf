eb63e0e4947af9d13f423fc4967cbabf5a1afd89
Joe <joew@joew.net>
2017-03-22 23:53:56 -0400

    test

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -8,7 +8,7 @@ variable "aws_region" {
 variable "aws_amis" {
   default = {
     "eu-west-1" = "ami-b1cf19c6"
-    "us-east-1" = "ami-1f3c9f09"
+    "us-east-1" = "ami-de7ab6b6"
     "us-west-1" = "ami-3f75767a"
     "us-west-2" = "ami-21f78e11"
   }
