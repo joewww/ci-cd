e67914fb5d28ff510ba0833c55cb1b4d1ce35abb
Joe <joew@joew.net>
2017-03-25 02:14:30 -0400

    switching to rhel

--- chefzero.sh
+++ chefzero.sh
@@ -4,8 +4,13 @@
 
 cd /tmp/
 
-curl -o"chef.deb" https://packages.chef.io/files/stable/chef/12.19.36/ubuntu/16.04/chef_12.19.36-1_i386.deb
+# Ubuntu
+#curl -o"chef.deb" https://packages.chef.io/files/stable/chef/12.19.36/ubuntu/16.04/chef_12.19.36-1_i386.deb
 
-sudo dpkg -i chef.deb
+# RHEL
+curl -o"chef.rpm" https://packages.chef.io/files/stable/chef/12.19.36/el/7/chef-12.19.36-1.el7.x86_64.rpm
+
+#sudo dpkg -i chef.deb
+sudo yum install chef.rpm -y
 
 cd /tmp/ci-cd/ && chef-client -z -o base | tee /tmp/chefrun.log
