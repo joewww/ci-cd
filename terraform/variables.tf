5556c47ff7545cfa855b2b7cd1de9a0fddd83354
Joe <joew@joew.net>
2017-03-25 02:15:15 -0400

    switching to rhel

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -4,13 +4,10 @@ variable "aws_region" {
   default     = "us-east-1"
 }
 
-# Ubuntu 
+# RHEL 7.3
 variable "aws_amis" {
   default = {
-    "eu-west-1" = "ami-b1cf19c6"
-    "us-east-1" = "ami-de7ab6b6"
-    "us-west-1" = "ami-3f75767a"
-    "us-west-2" = "ami-21f78e11"
+    "us-east-1" = "ami-b63769a1"
   }
 }
 
